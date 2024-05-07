6ce1237412b8ae514ff28e48623c6347265fe9a0
mehedi hasan <mdchanchal5675@gmail.com>
2024-05-07 10:30:27 +0600

    heijlas

--- Module_3/3.10.ts
+++ Module_3/3.10.ts
@@ -1,4 +1,31 @@
 {
   //encapsulation in oop
+
+  class BankAccount {
+    public name: string;
+    public readonly id: number;
+    protected _balance: number; //child a access dewar jnno private er replace hbe protected
+    constructor(name: string, id: number, _balance: number) {
+      this.name = name;
+      this.id = id;
+      this._balance = _balance;
+    }
+    addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+    get_Balance() {
+      return this._balance;
+    }
+  }
+  class StudentAccount extends BankAccount {
+    test() {
+      this._balance;
+    }
+  }
+  const goriberAccount = new BankAccount("mehedi", 1, 2000);
+  //   goriberAccount._balance = 2;
+  goriberAccount.addDeposit(20);
+  const my_Balance = goriberAccount.get_Balance();
+  console.log(my_Balance);
   //
 }
